c2fb4d04be1cb482e47d4c5ee636bb5852c0d014
DEVSAM1966 <desarrollo.devsam@gmail.com>
2025-09-23 19:32:45 +0200

    Create function sucess

---
+++ src/utilities/success.utility.ts
@@ -0,0 +1,16 @@
+export function success(value: any) {
+  const now = new Date();
+  const timestamp = new Intl.DateTimeFormat(undefined, {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  }).format(now);
+
+  return { "data": value,
+           "timestamp": timestamp
+   };
+}
