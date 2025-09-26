afc5bc0cdafafce6f5e2bbde8de8930ebd629d03
DEVSAM1966 <desarrollo.devsam@gmail.com>
2025-09-26 18:53:54 +0200

    Fix: include publishers.dto.ts from ignored out folder

---
+++ src/dtos/out/publishers.dto.ts
@@ -0,0 +1,11 @@
+export interface PublishersOutDTO {
+  publisher_id: number;
+  name_publisher: string;
+  address?: string;
+  city?: string;
+  province?: string;
+  postal_code?: string;
+  country?: string;
+  phone?: string;
+  notes?: string;
+}
