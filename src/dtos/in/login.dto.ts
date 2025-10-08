73fe735892b4eea2679d2638cd3113d03e6ce288
Ezetorc <ezetorc@gmail.com>
2025-10-08 18:05:32 -0300

    Add @IsStrongPassword validator

--- src/dtos/in/login.dto.ts
+++ src/dtos/in/login.dto.ts
@@ -1,11 +1,16 @@
-import { IsEmail, IsString, MaxLength } from "class-validator";
+import { IsEmail, IsStrongPassword, MaxLength } from "class-validator";
 
 export class LoginInDto {
   @IsEmail()
   @MaxLength(120)
   email: string;
 
-  @IsString()
+  @IsStrongPassword({
+    minLength: 8,
+    minLowercase: 2,
+    minUppercase: 2,
+    minNumbers: 1,
+  })
   @MaxLength(25)
   password: string;
 }
