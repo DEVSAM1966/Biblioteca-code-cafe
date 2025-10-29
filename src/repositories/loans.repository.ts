edc841b139ea555fb2a6d196291d7ed32501cfe2
DEVSAM1966 <desarrollo.devsam@gmail.com>
2025-10-29 22:11:22 +0100

    GET loans by isbn, user, loanDate

--- src/repositories/loans.repository.ts
+++ src/repositories/loans.repository.ts
@@ -10,4 +10,28 @@ export class LoansRepository {
         return await prisma.loan.findMany();
     }
 
+    static async getByIsbn(id: string): Promise<Loan[]> {
+        return await prisma.loan.findMany({ where: { isbn: id } });
+    }
+
+    static async getByUser(id: number): Promise<Loan[]> {
+        return await prisma.loan.findMany({ where: { userId: id } });
+    }
+
+    static async getByDate(date: Date): Promise<Loan[]> {
+        const start = new Date(date);
+        start.setHours(0, 0, 0, 0);
+        const end = new Date(date);
+        end.setHours(23, 59, 59, 999);
+
+        return await prisma.loan.findMany({
+            where: {
+                loanDate: {
+                    gte: start,
+                    lte: end,
+                },
+            },
+        });
+    }
+
 }
